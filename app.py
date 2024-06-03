d6110dc5ccbf480742d82ebb9eebada0860cdd63
HundSimon <77706174+HundSimon@users.noreply.github.com>
2024-06-03 21:33:22 +0800

    Rewrite with FastAPI

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request, render_template, redirect
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
 import json
 import random
 import re
 
-app = Flask(__name__)
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 with open("metadata.json", "r") as file:
     loaded_data = json.load(file)
@@ -14,11 +18,11 @@ with open("key.json", "r") as key:
     key_illust = key_loads["user_bookmarks_illust"]
     key_proxy_url = key_loads["proxy_url"]
 
-@app.route('/', methods=['GET'])
-def get_img():
+@app.get('/')
+async def get_img(request: Request):
     # Args
-    api_format = request.args.get("format")
-    api_nsfw = request.args.get("r18", default = "0")
+    api_format = request.query_params.get("format")
+    api_nsfw = request.query_params.get("r18", default = "0")
 
     # Get random data
     random_data = loaded_data[random.randint(0,len(loaded_data) - 1)]
@@ -34,28 +38,26 @@ def get_img():
                 "proxy_url": image_url_proxy,
                 "r18": random_data["r18"],
                 "user": random_data["user"]
-            }}
+                },
+            "code": 200
+            }
 
     # Filter r18 tag
     if api_nsfw == "0" and random_data["r18"] == 1:
-        return get_img()
+        return await get_img(request)
     if api_nsfw == "1" and random_data["r18"] == 0:
-        return get_img()
-    else:
-        pass
+        return await get_img(request)
 
     # Return different formats
-    if api_format == "json":
-        return jsonify(data)
-
-    elif api_format == "image":
-        return redirect(image_url_proxy, code=302)
+    if api_format == "image":
+        return RedirectResponse(url=image_url_proxy)
     else:
-        return jsonify(data)
+        return JSONResponse(content=data)
 
-@app.route('/index/')
-def index():
-    return render_template("index.html")
+@app.get('/index/')
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=8000)
